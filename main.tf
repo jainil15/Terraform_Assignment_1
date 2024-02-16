cba636824d1abe718dc342e1061a95bce16f0a9f
jainil15 <jainilpatel115@gmail.com>
2024-02-16 14:23:46 +0530

    Initial Commit without comments

--- main.tf
+++ main.tf
@@ -24,13 +24,49 @@ data "tls_public_key" "mykeypair_public" {
 
 locals {
   server_name  = "Jainils_Server"
+  vpc_name     = "Jainils_VPC"
   my_public_ip = jsondecode(data.http.my_public_ip.response_body).ip
 }
 
-resource "aws_default_vpc" "default_vpc" {
+resource "aws_vpc" "app_vpc" {
+  cidr_block       = "138.82.0.0/16"
+  instance_tenancy = "default"
   tags = {
-    Name = "Default"
+    Name = "${local.vpc_name}"
   }
+}
+
+resource "aws_subnet" "public_subnet_ap_south_1a" {
+  vpc_id            = aws_vpc.app_vpc.id
+  availability_zone = "ap-south-1a"
+  cidr_block        = "138.82.0.64/26"
+  depends_on        = [aws_vpc.app_vpc]
+  tags = {
+    Name = "Public_Subnet_1"
+  }
+}
+
+resource "aws_internet_gateway" "app_igw" {
+  vpc_id = aws_vpc.app_vpc.id
+  tags = {
+    Name = "Jainils_IGW"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.app_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app_igw.id
+  }
+  tags = {
+    Name = "Public_Route_Table_1"
+  }
+}
+
+resource "aws_route_table_association" "subnet_association_public_subnet_ap_south_1a" {
+  subnet_id      = aws_subnet.public_subnet_ap_south_1a.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_key_pair" "mykeypair" {
@@ -41,7 +77,7 @@ resource "aws_key_pair" "mykeypair" {
 resource "aws_security_group" "allow_http_and_ssh" {
   name        = "allow_http_and_ssh"
   description = "This security groups allows http and ssh inbound traffic from all sources"
-  vpc_id      = aws_default_vpc.default_vpc.id
+  vpc_id      = aws_vpc.app_vpc.id
 
   ingress {
     cidr_blocks      = ["0.0.0.0/0"]
@@ -69,7 +105,9 @@ resource "aws_instance" "app_server" {
   ami                    = "ami-06b72b3b2a773be2b"
   key_name               = aws_key_pair.mykeypair.key_name
   instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet_ap_south_1a.id
   vpc_security_group_ids = [aws_security_group.allow_http_and_ssh.id]
+  associate_public_ip_address = true
   tags = {
     Name = "${local.server_name}"
   }
@@ -82,7 +120,6 @@ systemctl enable httpd
 echo "<h1>HELLO WORLD FROM $(hostname -f)</h1>" > /var/www/html/index.html
 systemctl restart httpd
   EOF
-
 }
 
 output "instance_ip_address" {
